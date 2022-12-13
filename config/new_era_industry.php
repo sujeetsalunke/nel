16e2ddaf3584e379c86ae6564f60d86289e37d05
Sujeet Salunke <sujeetsalunke@gmail.com>
2022-12-13 12:47:39 +0530

    commit message

---
+++ config/new_era_industry.php
@@ -0,0 +1,6 @@
+<?php
+$new_era_industry['status'] = array(
+    1 => 'Active',
+    0 => 'Deactive'
+);
+$config['new_era_industry'] = $new_era_industry;
---
+++ config/system_admin.php
@@ -0,0 +1,106 @@
+<?php
+
+$config['1'] = array(
+    'Users' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Sliders' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Queries' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Brands' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Clients' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Projects' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'ProjectDetails' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Galleries' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Categories' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Materials' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Products' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'ProductDetails' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'CmsPages' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'Cities' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+    'AboutUs' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+     'ProductColorImages' => array(
+        0 => 'index',
+        1 => 'add',
+        2 => 'edit',
+        3 => 'delete',
+    ),
+);
+$config['roles'] = $config;
+?>
+
+
+
+
